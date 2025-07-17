8809924e74599ecf2b25148d4edde8caef631320
mennahassan8 <menna.hassan@zewailcity.edu.eg>
2025-07-17 23:01:50 +0300

    Clean repository for Svelte project

---
+++ my-svelte-app/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+
+const repoName = 'GIT-practice';
+
+export default defineConfig({
+  base: `/${repoName}/`,
+  plugins: [svelte()],
+});
+
+
