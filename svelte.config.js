7b1fde1e6ded99431f9a0d24bf0277437e90cd2c
Menna Hassan <s-menna.hassan@zewailcity.edu.eg>
2025-07-18 03:38:32 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 
 export default {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
+    adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/GIT-practice' : ''
+      base: '/GIT-practice'
     }
   }
 };
