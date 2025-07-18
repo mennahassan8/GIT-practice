24cb7a9143ea1d89043277428ec9434d6951dd30
Menna Hassan <s-menna.hassan@zewailcity.edu.eg>
2025-07-18 03:31:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
+// vite.config.ts
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default {
+  base: '/GIT-practice/', 
+  plugins: [sveltekit()]
+};
