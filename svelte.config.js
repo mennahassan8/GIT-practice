9ac992f58748c155dc68381982f6c3707d0fd75e
Menna Hassan <s-menna.hassan@zewailcity.edu.eg>
2025-07-18 03:46:08 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
+const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
     paths: {
@@ -8,4 +10,5 @@ export default {
     }
   }
 };
- 
+
+export default config;
