b5a25b8b674eea64348fb70402003d5232b1b8c1
mennahassan8 <menna.hassan@zewailcity.edu.eg>
2025-07-19 15:58:34 +0300

    svelte web update

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,6 @@
-import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html'
-    }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/GIT-practice' : ''
-    }
-  },
-  preprocess: vitePreprocess()
+  
+  preprocess: vitePreprocess(),
 }
